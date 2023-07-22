0183fe14601508c429c37ef8acb49771dc10a68d
Jimmy Chuang <ogre0403@gmail.com>
2023-07-22 16:34:16 +0800

    refactor: refactor network configurations in helper scripts
    - Change the floating IP and gateway addresses to `10.0.2.x`
    - Add a bridge mapping function to `helper.sh`
    - Remove the last newline character in `teardown.sh`
    
    Signed-off-by: Jimmy Chuang <ogre0403@gmail.com>

--- day-06/helper.sh
+++ day-06/helper.sh
@@ -140,6 +140,10 @@ function add-bridge-mapping {
     ip link set $1 up 
     ovs-vsctl set Open_vSwitch . external-ids:ovn-bridge-mappings=$2:$br_name
 
+    # assign ip to bridge, otherwise it will not be able to communicate with external network
+    # https://medium.com/@john.lin/%E9%80%8F%E9%81%8E-ovs-bridge-%E5%8F%8A-docker-%E8%A6%AA%E6%89%8B%E6%89%93%E9%80%A0-sdn-%E5%AF%A6%E9%A9%97%E7%B6%B2%E8%B7%AF-%E5%9B%9B-%E8%A8%AD%E5%AE%9A%E5%A4%96%E9%83%A8%E9%80%A3%E7%B6%B2-b0e96d9d2ee1
+    ifconfig $1 0
+    dhclient $br_name
 }
 
 #arg1: logical router name
--- day-06/sample.sh
+++ day-06/sample.sh
@@ -2,9 +2,9 @@
 
 source helper.sh
 
-export EXTERNAL_GW=192.168.121.1
-export FLOATING_IP=192.168.121.111
-export EXTERNAL_IP=192.168.121.100
+export EXTERNAL_GW=10.0.2.2
+export FLOATING_IP=10.0.2.200
+export EXTERNAL_IP=10.0.2.100
 
 create-ns ns1 192.168.100.100 192.168.100.254
 create-ovn-ls-and-lsp   ls0 ns1
